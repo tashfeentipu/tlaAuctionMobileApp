70131272c622495411e54dd8165d7a48f84052eb
tashfeentipu <tashfeentipu@hotmail.com>
2022-04-23 18:21:25 +0100

    --commit: Bottom Tab Navigatort work started--

--- src/Navigation/TabNavigator.tsx
+++ src/Navigation/TabNavigator.tsx
@@ -1,9 +1,12 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { Component } from "react";
+import { Image } from 'react-native';
+import Home from '../Screens/Home';
 import Profile from "../Screens/Profile";
 import Rewards from "../Screens/Rewards";
-import Splash from "../Screens/Splash";
+import Settings from '../Screens/Settings';
 import Wallet from "../Screens/Wallet";
+import * as RouteNames from "./Routes";
 
 const Tab = createBottomTabNavigator();
 
@@ -15,6 +18,12 @@ interface IState {
 
 }
 
+const HomeIcon = require("../Assets/Home.png")
+const Account = require("../Assets/Account.png")
+const RewardsIcon = require("../Assets/Rewards.png")
+const WalletIcon = require("../Assets/Wallet.png")
+const SettingsIcon = require("../Assets/Settings.png")
+
 class BottomTabNavigator extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
@@ -23,11 +32,36 @@ class BottomTabNavigator extends Component<IProps, IState> {
 
     render() {
         return (
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={Splash} />
-                <Tab.Screen name="Wallet" component={Wallet} />
-                <Tab.Screen name="Rewards" component={Rewards} />
-                <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Navigator >
+                <Tab.Screen name={RouteNames.HOME} component={Home} options={{
+                    tabBarIcon: (color: string, size: number) => (<Image source={HomeIcon} />)
+
+
+                }} />
+                <Tab.Screen name={RouteNames.WALLET} component={Wallet}
+                    options={{
+                        tabBarIcon: () =>
+                            <Image source={HomeIcon} />
+
+                    }} />
+                <Tab.Screen name={RouteNames.REWARDS} component={Rewards}
+                    options={{
+                        tabBarIcon: () =>
+                            <Image source={HomeIcon} />
+
+                    }} />
+                <Tab.Screen name={RouteNames.PROFILE} component={Profile}
+                    options={{
+                        tabBarIcon: () =>
+                            <Image source={HomeIcon} />
+
+                    }} />
+                <Tab.Screen name={RouteNames.SETTINGS} component={Settings}
+                    options={{
+                        tabBarIcon: () => {
+                            return <Image source={HomeIcon} />
+                        }
+                    }} />
             </Tab.Navigator>
         );
     }
