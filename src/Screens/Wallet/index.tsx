855a596b67784107a75605e138fd9bb3c3d58a93
tashfeentipu <tashfeentipu@hotmail.com>
2022-04-30 20:03:04 +0100

    --commit: Wallet Work Started--

--- src/Screens/Wallet/index.tsx
+++ src/Screens/Wallet/index.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import { Metrics } from "../../Theme";
 import { Colors } from "../../Theme/Colors";
 interface IProps {
 
@@ -17,17 +19,45 @@ class Wallet extends Component<IProps, IState> {
 
     render() {
         return (
-            <View style={styles.MainContainer} >
-                <Text>Wallet</Text>
-            </View>
+            <ScrollView style={styles.MainContainerStyle} contentContainerStyle={styles.MainContainer} >
+                <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} colors={[Colors.purple1, Colors.purple2]} style={styles.GradientContainer}>
+
+                </LinearGradient>
+                <View style={styles.MarketContainer} >
+                    <Text style={styles.MarketHeading} >
+                        Market
+                    </Text>
+
+                </View>
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    MainContainer: {
-        flex: 1,
+    MainContainerStyle: {
         backgroundColor: Colors.white
+    },
+    MainContainer: {
+        alignItems: 'center',
+        backgroundColor: Colors.white
+    },
+    GradientContainer: {
+        width: Metrics.WIDTH * 0.873,
+        height: Metrics.HEIGHT * 0.325,
+        borderRadius: Metrics.WIDTH * 0.1,
+        paddingTop: Metrics.WIDTH * 0.1
+    },
+    MarketContainer: {
+        marginTop: Metrics.HEIGHT * 0.05,
+        borderTopLeftRadius: Metrics.HEIGHT * 0.05,
+        borderTopRightRadius: Metrics.HEIGHT * 0.05,
+        width: Metrics.WIDTH,
+        height: Metrics.HEIGHT * 0.4,
+        backgroundColor: Colors.skyBlue
+    },
+    MarketHeading: {
+        fontSize: Metrics.WIDTH * 0.08
     }
 });
 
