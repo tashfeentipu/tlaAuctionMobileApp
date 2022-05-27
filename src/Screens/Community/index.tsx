966d3e6cadfd197dd2e55e507e6b9ef28ccd50d9
tashfeentipu <tashfeentipu@hotmail.com>
2022-05-27 21:39:30 +0100

    --commit: Icons added for community screen and useless icons are deleted--

--- src/Screens/Community/index.tsx
+++ src/Screens/Community/index.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Colors, Fonts, Metrics } from "../../Theme";
 
 interface IProps {
 
@@ -18,14 +19,30 @@ class Community extends Component<IProps, IState> {
     render() {
         return (
             <View style={styles.MainContainer} >
-                <Text>Community</Text>
+                <View style={styles.MainContainer2}  >
+                    <View>
+                        <Text style={styles.HeadingText}>Connect with thousand of other PolkaStarterPad users to discuss and share anything about crypto knowledge.</Text>
+                    </View>
+                    
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    MainContainer: {}
+    MainContainer: {
+        alignItems: "center",
+        flex: 1
+    },
+    MainContainer2: {
+        width: Metrics.WIDTH * 0.9
+    },
+    HeadingText: {
+        color: Colors.grey3,
+        fontFamily: Fonts.Regular,
+        fontSize: Metrics.WIDTH * 0.04
+    }
 });
 
 export default Community;
